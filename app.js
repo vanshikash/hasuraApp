acd93e7b05835428fe3d63494e4dc232fcc991cc
Vanshika <78431790+vanshikash@users.noreply.github.com>
2022-10-28 12:09:39 +0530

    Added new route
    Added a new route welcome

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const cookieParser = require("cookie-parser")
 const hasuraRoutes = require("./routes/hasura")
 
 const app = express();
-console.log("Starting server at port 3000")
+console.log("Starting server at port 4000")
 app.listen(4000);
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
+
+const rout = express.Router();
+app.use(rout)
+router.get('/welcome', (req, res) => {
+    console.log("Check #1");
+    res.json({ message: "Welcome to this new page" });
+})
 
 app.use(hasuraRoutes);
